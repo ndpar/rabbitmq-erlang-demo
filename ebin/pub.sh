7175a06ce9f53123b27c74cfdac315f536962478
Andrey Paramonov <git@ndpar.com>
2010-09-21 23:07:30 -0400

    send multiple messages

--- ebin/pub.sh
+++ ebin/pub.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-
+#
+# Usage: ./pub.sh 100
+#
 export ERL_LIBS=$RABBITMQ_HOME/rabbitmq-erlang-client/dist
 
-erl -noshell -s rabbitmq_pub main -s init stop
+erl -noshell -run rabbitmq_pub send $1 -s init stop
 
--- src/rabbitmq_pub.erl
+++ src/rabbitmq_pub.erl
@@ -3,22 +3,33 @@
 
 -include_lib("amqp_client/include/amqp_client.hrl").
 
-main() ->
+send(Args) ->
     Connection = amqp_connection:start_network(#amqp_params{host = "lab.ndpar.com"}),
     Channel = amqp_connection:open_channel(Connection),
     Exchange = <<"myExchange">>,
     Routing = <<"myRoutingKey">>,
     Payload = <<"Hello from Erlang!">>,
 
-    ok = basic_publish(Channel, Exchange, Routing, Payload),
+    statistics(runtime),
+    statistics(wall_clock),
+
+    N = list_to_integer(hd(Args)),
+    for(1, N, fun() -> basic_publish(Channel, Exchange, Routing, Payload) end),
 
     amqp_channel:close(Channel),
     amqp_connection:close(Connection),
-    ok.
+
+    {_, Time1} = statistics(runtime),
+    {_, Time2} = statistics(wall_clock),
+    io:format("Send time=~p (~p) nanoseconds~n", [Time1 / N, Time2 / N]).
+
 
 basic_publish(Channel, Exchange, Routing, Payload) -> 
     Publish = #'basic.publish'{exchange = Exchange, routing_key = Routing},
     Message = #amqp_msg{payload = Payload},
-    ok = amqp_channel:cast(Channel, Publish, Message),
-    ok.
+    amqp_channel:cast(Channel, Publish, Message).
+
+
+for(N, N, F) -> F();
+for(I, N, F) -> F(), for(I+1, N, F).
 
